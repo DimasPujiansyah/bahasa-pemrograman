00c1692a4b19e3221b97539ccd2db48d022c9879
root <root@LAPTOP-63TMSS04>
2024-05-25 08:57:18 +0700

    perubahan

---
+++ uts/soal4.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int usia = 21;
+    
+    // Menampilkan hasil
+    cout << "Berarti Usia Kalian Sekarang adalah " << usia << " Tahun" << endl;
+    
+    return 0;
+}
